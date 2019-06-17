a98c1acfb0cc95bbd9b0b121350bee1ace63966c
Nataliaado <Nataliaado@DESKTOP-PD8C9RP>
2019-06-16 22:43:33 -0300

    Correção na atividade 7.

---
+++ Senac-dd-20191-Natalia/src/exercicio7/controller/ProdutoController.java
@@ -0,0 +1,71 @@
+package exercicio7.controller;
+
+import java.util.List;
+
+import exercicio7.model.bo.ProdutoBO;
+import exercicio7.model.seletor.ProdutoSeletor;
+import exercicio7.model.vo.Produto;
+
+public class ProdutoController {
+
+	public static final String TIPO_RELATORIO_XLS = "xls";
+	public static final String TIPO_RELATORIO_PDF = "pdf";
+
+	private ProdutoBO produtoBO = new ProdutoBO();
+
+	public List<Produto> listarTodosProdutos() {
+		return produtoBO.listarProdutos();
+	}
+
+	public List<Produto> listarProdutosAtePreco(double maximo) {
+		return produtoBO.listarProdutosPorFaixaDePreco(maximo);
+	}
+
+	public String salvar(Produto produto) {
+		String validacao = validarProduto(produto);
+
+		if (validacao == "") {
+			if (produto.getId() > 0) {
+				if (produtoBO.atualizar(produto)) {
+					validacao = "Produto atualizado com sucesso!";
+				} else {
+					validacao = "Erro ao atualizar produto";
+				}
+			} else {
+				if (produtoBO.inserir(produto)) {
+					validacao = "Produto salvo com sucesso!";
+				} else {
+					validacao = "Erro ao salvar produto";
+				}
+			}
+		}
+
+		return validacao;
+	}
+
+	private String validarProduto(Produto produto) {
+		String validacao = "";
+
+		if (produto == null) {
+			validacao = "- Produto está NULO!";
+		} else {
+			if (produto.getNome().trim().equals("")) {
+				validacao += "- Nome é obrigatório \n";
+			}
+			if (produto.getFabricante().trim().equals("")) {
+				validacao += "- Fabricante é obrigatório \n";
+			}
+		}
+		return validacao;
+	}
+
+	public void gerarRelatorio(List<Produto> produtos, String caminhoEscolhido, String tipoRelatorio) {
+		if (tipoRelatorio.equals(TIPO_RELATORIO_XLS)) {
+			produtoBO.gerarPlanilha(produtos, caminhoEscolhido);
+		}
+	}
+
+	public List<Produto> listarProdutos(ProdutoSeletor seletor) {
+		return produtoBO.listarProdutos(seletor);
+	}
+}
---
+++ Senac-dd-20191-Natalia/src/exercicio7/model/bo/ProdutoBO.java
@@ -0,0 +1,46 @@
+package exercicio7.model.bo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import exercicio7.model.dao.ProdutoDAO;
+import exercicio7.model.seletor.ProdutoSeletor;
+import exercicio7.model.vo.Produto;
+
+public class ProdutoBO {
+
+	ProdutoDAO produtoDAO = new ProdutoDAO();
+
+	public boolean inserir(Produto produto) {
+		int idGerado = produtoDAO.inserir(produto);
+		return idGerado > 0;
+	}
+
+	public exercicio7.model.vo.Produto buscarProdutoPorId(String textoId) {
+		Produto produtoBuscar = produtoDAO.obterPorId(Integer.parseInt(textoId));
+		return produtoBuscar;
+	}
+
+	public boolean atualizar(Produto produto) {
+		boolean sucesso = produtoDAO.atualizar(produto);
+		return sucesso;
+	}
+
+	public List<Produto> listarProdutos() {
+		ArrayList<Produto> produtos = produtoDAO.listarTodos();
+		return produtos;
+	}
+
+	public List<Produto> listarProdutosPorFaixaDePreco(double precoMax) {
+		ArrayList<Produto> produtos = produtoDAO.listarPorFaixaDePreco(precoMax);
+		return produtos;
+	}
+
+	public void gerarPlanilha(List<Produto> produtos, String caminhoEscolhido) {
+		// TODO
+	}
+
+	public List<Produto> listarProdutos(ProdutoSeletor seletor) {
+		return produtoDAO.listarComSeletor(seletor);
+	}
+}
