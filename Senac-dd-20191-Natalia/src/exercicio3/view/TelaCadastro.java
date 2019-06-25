dadd06df75a381d88aed6dae8e489f769163e3f4
Nataliaado <Nataliaado@DESKTOP-PD8C9RP>
2019-06-25 15:19:47 -0300

    Correção no exercício.

--- Senac-dd-20191-Natalia/src/exercicio3/view/TelaCadastro.java
+++ Senac-dd-20191-Natalia/src/exercicio3/view/TelaCadastro.java
@@ -19,6 +19,9 @@ import javax.swing.SwingConstants;
 import exercicio3.controller.Controller;
 import exercicio3.model.bo.NivelBO;
 import exercicio3.model.vo.NivelVO;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.GridLayout;
 
 public class TelaCadastro {
 
@@ -63,84 +66,106 @@ public class TelaCadastro {
 		frmCadastroDeUsuarios.setTitle("Cadastro de usuários");
 		frmCadastroDeUsuarios.setBounds(100, 100, 407, 256);
 		frmCadastroDeUsuarios.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmCadastroDeUsuarios.getContentPane().setLayout(null);
 
 		JLabel lblNome = new JLabel("Nome:");
-		lblNome.setBounds(20, 20, 55, 15);
+		frmCadastroDeUsuarios.getContentPane().setLayout(new GridLayout(0, 4, 0, 0));
 		frmCadastroDeUsuarios.getContentPane().add(lblNome);
+				
+						pfConfirmacaoSenha = new JPasswordField();
+						frmCadastroDeUsuarios.getContentPane().add(pfConfirmacaoSenha);
+				
+						JLabel lblSenha = new JLabel("Senha:");
+						frmCadastroDeUsuarios.getContentPane().add(lblSenha);
+				
+						pfSenha = new JPasswordField();
+						frmCadastroDeUsuarios.getContentPane().add(pfSenha);
+				
+						JLabel lblConfirmaoDeSenha = new JLabel("Confirmação:");
+						frmCadastroDeUsuarios.getContentPane().add(lblConfirmaoDeSenha);
+				
+						txtNome = new JTextField();
+						txtNome.setColumns(10);
+						frmCadastroDeUsuarios.getContentPane().add(txtNome);
+				
+						JLabel lblNivel = new JLabel("Nível:");
+						frmCadastroDeUsuarios.getContentPane().add(lblNivel);
+				
+						// Novo componente: Combobox
+						cbNivel = new JComboBox();
+						cbNivel.setModel((ComboBoxModel) new DefaultComboBoxModel(((java.util.List<NivelVO>) niveis).toArray()));
+						
+								// Inicia sem nada selecionado no combo
+								cbNivel.setSelectedIndex(-1);
+								frmCadastroDeUsuarios.getContentPane().add(cbNivel);
+				
+						JLabel lblEmail = new JLabel("Email:");
+						frmCadastroDeUsuarios.getContentPane().add(lblEmail);
+				
+						txtEmail = new JTextField();
+						txtEmail.setColumns(10);
+						frmCadastroDeUsuarios.getContentPane().add(txtEmail);
+				
+				JLabel label = new JLabel("");
+				frmCadastroDeUsuarios.getContentPane().add(label);
+				
+				JLabel label_1 = new JLabel("");
+				frmCadastroDeUsuarios.getContentPane().add(label_1);
+				
+				JLabel label_2 = new JLabel("");
+				frmCadastroDeUsuarios.getContentPane().add(label_2);
+				
+				JLabel label_3 = new JLabel("");
+				frmCadastroDeUsuarios.getContentPane().add(label_3);
+				
+				JLabel label_4 = new JLabel("");
+				frmCadastroDeUsuarios.getContentPane().add(label_4);
+				
+				JLabel label_5 = new JLabel("");
+				frmCadastroDeUsuarios.getContentPane().add(label_5);
+				
+				JLabel label_6 = new JLabel("");
+				frmCadastroDeUsuarios.getContentPane().add(label_6);
+				
+						JButton button = new JButton("Salvar");
+						button.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								String senha = new String(pfSenha.getPassword());
+								String confirmacaoSenha = new String(pfConfirmacaoSenha.getPassword());
+								NivelVO nivel = (NivelVO) cbNivel.getModel().getSelectedItem();
 
-		JLabel lblEmail = new JLabel("Email:");
-		lblEmail.setBounds(20, 55, 55, 15);
-		frmCadastroDeUsuarios.getContentPane().add(lblEmail);
-
-		JLabel lblSenha = new JLabel("Senha:");
-		lblSenha.setBounds(20, 90, 55, 15);
-		frmCadastroDeUsuarios.getContentPane().add(lblSenha);
-
-		JLabel lblConfirmaoDeSenha = new JLabel("Confirmação:");
-		lblConfirmaoDeSenha.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblConfirmaoDeSenha.setBounds(165, 90, 94, 16);
-		frmCadastroDeUsuarios.getContentPane().add(lblConfirmaoDeSenha);
-
-		JLabel lblNivel = new JLabel("Nível:");
-		lblNivel.setBounds(20, 125, 55, 15);
-		frmCadastroDeUsuarios.getContentPane().add(lblNivel);
-
-		txtNome = new JTextField();
-		txtNome.setBounds(70, 15, 280, 28);
-		frmCadastroDeUsuarios.getContentPane().add(txtNome);
-		txtNome.setColumns(10);
-
-		txtEmail = new JTextField();
-		txtEmail.setBounds(70, 50, 280, 28);
-		frmCadastroDeUsuarios.getContentPane().add(txtEmail);
-		txtEmail.setColumns(10);
-
-		pfSenha = new JPasswordField();
-		pfSenha.setBounds(70, 85, 90, 28);
-		frmCadastroDeUsuarios.getContentPane().add(pfSenha);
-
-		pfConfirmacaoSenha = new JPasswordField();
-		pfConfirmacaoSenha.setBounds(260, 84, 90, 28);
-		frmCadastroDeUsuarios.getContentPane().add(pfConfirmacaoSenha);
-
-		// Novo componente: Combobox
-		cbNivel = new JComboBox();
-		cbNivel.setModel((ComboBoxModel) new DefaultComboBoxModel(((java.util.List<NivelVO>) niveis).toArray()));
-
-		// Inicia sem nada selecionado no combo
-		cbNivel.setSelectedIndex(-1);
-
-		cbNivel.setBounds(70, 120, 280, 28);
-		frmCadastroDeUsuarios.getContentPane().add(cbNivel);
-
-		JButton btnLimpar = new JButton("Limpar");
-		btnLimpar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				txtNome.setText("");
-				txtEmail.setText("");
-				pfSenha.setText("");
-				pfConfirmacaoSenha.setText("");
-				cbNivel.setSelectedIndex(-1);
-			}
-		});
-		btnLimpar.setBounds(190, 155, 160, 35);
-		frmCadastroDeUsuarios.getContentPane().add(btnLimpar);
-
-		JButton button = new JButton("Salvar");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String senha = new String(pfSenha.getPassword());
-				String confirmacaoSenha = new String(pfConfirmacaoSenha.getPassword());
-				NivelVO nivel = (NivelVO) cbNivel.getModel().getSelectedItem();
-
-				Controller controller = new Controller();
-				JOptionPane.showMessageDialog(null,
-						controller.salvar(txtNome.getText(), txtEmail.getText(), nivel, confirmacaoSenha, senha));
-			}
-		});
-		button.setBounds(20, 155, 160, 35);
-		frmCadastroDeUsuarios.getContentPane().add(button);
+								Controller controller = new Controller();
+								JOptionPane.showMessageDialog(null,
+										controller.salvar(txtNome.getText(), txtEmail.getText(), nivel, confirmacaoSenha, senha));
+							}
+						});
+						frmCadastroDeUsuarios.getContentPane().add(button);
+				
+						JButton btnLimpar = new JButton("Limpar");
+						btnLimpar.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								txtNome.setText("");
+								txtEmail.setText("");
+								pfSenha.setText("");
+								pfConfirmacaoSenha.setText("");
+								cbNivel.setSelectedIndex(-1);
+							}
+						});
+						frmCadastroDeUsuarios.getContentPane().add(btnLimpar);
+				
+				JLabel label_7 = new JLabel("");
+				frmCadastroDeUsuarios.getContentPane().add(label_7);
+				
+				JLabel label_8 = new JLabel("");
+				frmCadastroDeUsuarios.getContentPane().add(label_8);
+				
+				JLabel label_9 = new JLabel("");
+				frmCadastroDeUsuarios.getContentPane().add(label_9);
+				
+				JLabel label_10 = new JLabel("");
+				frmCadastroDeUsuarios.getContentPane().add(label_10);
+				
+				JLabel label_11 = new JLabel("");
+				frmCadastroDeUsuarios.getContentPane().add(label_11);
 	}
 
 	private void consultarNiveis() {
